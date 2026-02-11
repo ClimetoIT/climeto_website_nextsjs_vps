b3025dc44d2fc244f4d7fad834ed7eae53abecff
Anupriya kushwaha <it.climeto@gmail.com>
2026-02-11 17:46:36 +0530

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true, // Required for static export
   },
+ 
 };
 
 export default nextConfig;
